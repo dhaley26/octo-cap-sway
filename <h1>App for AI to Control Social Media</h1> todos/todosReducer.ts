b4f12d36ac4d40fb422cd98831eee9dfd84af4ed
dhaley26 <140873504+dhaley26@users.noreply.github.com>
2023-07-29 00:21:35 -0500

    Create todosReducer.ts

---
+++ <h1>App for AI to Control Social Media</h1> todos/todosReducer.ts
@@ -0,0 +1,50 @@
+// file: todos/todosReducer.ts noEmit
+import type { Reducer } from '@reduxjs/toolkit'
+declare const reducer: Reducer<{}>
+export default reducer
+
+// file: visibility/visibilityReducer.ts noEmit
+import type { Reducer } from '@reduxjs/toolkit'
+declare const reducer: Reducer<{}>
+export default reducer
+
+// file: store.ts
+import { configureStore } from '@reduxjs/toolkit'
+
+// We'll use redux-logger just as an example of adding another middleware
+import logger from 'redux-logger'
+
+// And use redux-batched-subscribe as an example of adding enhancers
+import { batchedSubscribe } from 'redux-batched-subscribe'
+
+import todosReducer from './todos/todosReducer'
+import visibilityReducer from './visibility/visibilityReducer'
+
+const reducer = {
+  todos: todosReducer,
+  visibility: visibilityReducer,
+}
+
+const preloadedState = {
+  todos: [
+    {
+      text: 'Eat food',
+      completed: true,
+    },
+    {
+      text: 'Exercise',
+      completed: false,
+    },
+  ],
+  visibilityFilter: 'SHOW_COMPLETED',
+}
+
+const debounceNotify = _.debounce((notify) => notify())
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState,
+  enhancers: [batchedSubscribe(debounceNotify)],
+})
